039ba3dd29a5499187444c93c630056f01563980
Lihkin <shniks@gmail.com>
2018-02-21 09:14:21 -0700

    Creating activity class test and associated code.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
 class Activity
+
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = []
+  end
+
+  def add_participant(participant)
+    participants << participant
+  end 
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,39 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_if_it_exists
+    activity = Activity.new("hiking")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_if_it_can_take_a_name
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_if_it_has_an_initial_empty_participants_array
+    activity = Activity.new("hiking")
+
+    assert_equal [], activity.participants
+  end
+
+  def test_if_it_can_add_participants
+    activity = Activity.new("hiking")
+    activity.add_participant({"Nikhil" => 32})
+
+    assert_equal [{"Nikhil" => 32}], activity.participants
+  end
+
+  # def test_if_it_can_add_two_participants
+  #   activity = Activity.new("hiking")
+  #   activity.add_participant({"Nikhil" => 32})
+  #   activity.add_participant({"Adrian" => 20})
+  #
+  #   assert_equal [{"Nikhil" => 32}, {"Adrian" => 20}], activity.participants
+  # end
+
+
 end
