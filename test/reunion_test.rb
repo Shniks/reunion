e568eed9935e219366398b6405f83b0113229466
Lihkin <shniks@gmail.com>
2018-02-21 09:38:02 -0700

    Initial commit iteration 3.

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,11 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_if_it_exists
+    reunion = Reunion.new
+
+    assert_instance_of Reunion, reunion
+  end
+  
 end
