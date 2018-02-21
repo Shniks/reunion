d0f1a3723d3a67e9e9fa914fcbe03143874e3900
Lihkin <shniks@gmail.com>
2018-02-21 09:43:41 -0700

    Completing iteratoin 3.

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,15 @@
 class Reunion
+
+  attr_reader :location,
+              :activities
+
+  def initialize(location)
+    @location = location
+    @activities = []
+  end
+
+  def add_activity(activity)
+    activities << activity
+  end 
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -6,9 +6,36 @@ require './lib/reunion'
 class ReunionTest < Minitest::Test
 
   def test_if_it_exists
-    reunion = Reunion.new
+    reunion = Reunion.new("Hawaii")
 
     assert_instance_of Reunion, reunion
   end
-  
+
+  def test_reunion_has_a_location
+    reunion = Reunion.new("Hawaii")
+
+    assert_equal "Hawaii", reunion.location
+  end
+
+  def test_reunion_can_take_another_location
+    reunion = Reunion.new("Palm Springs")
+
+    assert_equal "Palm Springs", reunion.location
+  end
+
+  def test_reunion_has_an_empty_activities_array
+    reunion = Reunion.new("Hawaii")
+
+    assert_equal [], reunion.activities
+  end
+
+  def test_add_activities_can_add_activities
+    reunion = Reunion.new("Hawaii")
+    activity = Activity.new("Hiking")
+    reunion.add_activity(activity)
+
+    assert_equal [activity], reunion.activities
+    assert_instance_of Activity, reunion.activities.first
+  end
+
 end
